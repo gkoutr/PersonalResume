b4f0a8cfaae277ae88972ef342fd3a10b7070244
George Koutroumpis <gjkoutroumpis@gmail.com>
2020-05-19 11:14:16 -0400

    Add project files.

---
+++ PersonalResume/Models/About.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PersonalResume.Models
+{
+    public class About
+    {
+        public String Name { get; set; }
+        public String Title { get; set; }
+        public String Location { get; set; }
+        public String Description { get; set; }
+    }
+}
