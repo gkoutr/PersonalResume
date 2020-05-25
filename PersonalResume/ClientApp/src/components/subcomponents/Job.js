e0053003745815232d7c90861f5bd9264ad48857
George Koutroumpis <gjkoutroumpis@gmail.com>
2020-05-25 13:16:43 -0400

    Updated Job component to be a functional component.

--- PersonalResume/ClientApp/src/components/subcomponents/Job.js
+++ PersonalResume/ClientApp/src/components/subcomponents/Job.js
@@ -5,39 +5,37 @@ import {
 } from 'reactstrap';
 import './Job.css';
 
-//props are properties that come from a parent component, in this case Jobs. props will not change, so we can use them directly in our render function
-export default class Job extends Component {
-    constructor(props) {
-        super(props);
-    }
+function Job(props) {
 
-    render() {
-        return (
-            <div className="job">
-                <Card className="job-card">
-                    <CardBody>
-                        <Row>
-                            <Col md={7} sm={12}>
-                                <CardImg className="company-logo center-mobile smaller-width" top width="100%" src={require('../../images/jobs/' + this.props.job.logo)} />
-                            </Col>
-                            <Col md={5} sm={12}>
-                                <div className="center-job-info">
-                                    <CardTitle className="h4">{this.props.job.companyName}</CardTitle>
-                                    <CardSubtitle className="job-title">{this.props.job.jobTitle}</CardSubtitle>
-                                    <CardText>{this.props.job.startDate} – {this.props.job.endDate}</CardText>
-                                    <CardText className="job-location">{this.props.job.location}</CardText>
-                                </div>
-                            </Col>
-                        </Row>
-                        <h5 className="text-center">Job Duties</h5>
-                        <ol>
-                            {this.props.job.jobDuties.map(duty =>
-                                <li key={duty}>{duty}</li>
-                            )}
-                        </ol>
-                    </CardBody>
-                </Card>
-            </div>
-        );
-    }
+    const job = props.job;
+
+    return (
+        <div className="job">
+            <Card className="job-card">
+                <CardBody>
+                    <Row>
+                        <Col md={7} sm={12}>
+                            <CardImg className="company-logo center-mobile smaller-width" top width="100%" src={require('../../images/jobs/' + job.logo)} />
+                        </Col>
+                        <Col md={5} sm={12}>
+                            <div className="center-job-info">
+                                <CardTitle className="h4">{job.companyName}</CardTitle>
+                                <CardSubtitle className="job-title">{job.jobTitle}</CardSubtitle>
+                                <CardText>{job.startDate} – {job.endDate}</CardText>
+                                <CardText className="job-location">{job.location}</CardText>
+                            </div>
+                        </Col>
+                    </Row>
+                    <h5 className="text-center">Job Duties</h5>
+                    <ol>
+                        {job.jobDuties.map(duty =>
+                            <li key={duty}>{duty}</li>
+                        )}
+                    </ol>
+                </CardBody>
+            </Card>
+        </div>
+    );
 }
+
+export default Job;
